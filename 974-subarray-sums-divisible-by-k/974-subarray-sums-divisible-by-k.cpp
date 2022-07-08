ba9a5dad0f037335d04b206ff65d2da1fc7d9ff6
pr201an <102686507+pr201an@users.noreply.github.com>
2022-07-08 11:44:36 +0530

    Time: 532 ms (5.01%), Space: 34.4 MB (5.28%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        unordered_map<int, int> map;
         for(int i=1; i<nums.size(); i++){
             nums[i] += nums[i-1];
         }
+        unordered_map<int, int> map;
         map[0] = 1;
-        int count = 0;
+        int count =0;
         for(int i=0; i<nums.size(); i++){
             if(map[nums[i]%k] != 0){
-                count = count + map[nums[i]%k];
-                cout << i << " " << count <<  endl;
+                count += map[nums[i]%k];
+                cout << count << " " << i << endl; 
             }
-            if(map[nums[i]%k + k] != 0){
-                count = count + map[nums[i]%k + k];
-                cout << i << " " << count <<  endl;
+            if(map[(nums[i]%k)+k] != 0){
+                count += map[(nums[i]%k)+k];
+                cout << count << " " << i << endl; 
             }
             if(nums[i] < 0)
-                map[nums[i]%k + k]++;
+                map[(nums[i]%k) + k]++;
             else
                 map[nums[i]%k]++;
         }
