fd3f18216b1b04c2742bbd9e036e68c91113fcd9
pr201an <102686507+pr201an@users.noreply.github.com>
2022-08-21 22:56:33 +0530

    Time: 434 ms (5.05%), Space: 58 MB (57.38%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -5,11 +5,13 @@ public:
         unordered_map<int,int> mp;
         for(int i=0;i<arr.size();i++) mp[arr[i]]++;
         for(int i=0;i<arr.size();i++){
-            if(mp[arr[i]]==0);
+            if(mp[arr[i]]==0){
+                cout << arr[i] << " " << mp[arr[i]] << endl;
+            }
             else{
                 int x=2*arr[i];
-                if(arr[i]<0 && abs(arr[i])%2==0) x=arr[i]/2;
-                if(mp.find(x)!=mp.end())
+                //if(arr[i]<0 && abs(arr[i])%2==0) x=arr[i]/2;
+                if(mp[x] != 0)
                 {
                     mp[x]--;
                     mp[arr[i]]--;
@@ -17,7 +19,9 @@ public:
             }
         }
         for(auto i:mp){
-            if(i.second) return false;
+            if(i.second){
+                return false;
+            }
         }
         return true;
     }
