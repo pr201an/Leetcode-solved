4dc81c38a58f4aa0124554efb85821f2522a9756
pr201an <102686507+pr201an@users.noreply.github.com>
2022-10-23 21:25:12 +0530

    Time: 315 ms (23.10%), Space: 83.8 MB (84.15%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++){
+            if(i == 0 && nums[i] == 1)
+                continue;
+            if(i == 0 && nums[i] == 0){
+                nums[0] = -1;
+                continue;
+            }
+            if(nums[i] == 0)
+                nums[i] = nums[i-1]-1;
+            else
+                nums[i] = nums[i-1]+1;
+        }
+        int ans = 0;
+        unordered_map<int, int> ump;
+        ump[0] = -1;
+        for(int i=0; i<nums.size(); i++){
+            if(ump.find(nums[i]) == ump.end())
+                ump[nums[i]] = i;
+            else
+                ans = max(ans, i-ump[nums[i]]);
+        }
+        // vector<int>::iterator it;
+        // if(ans == 2){
+        //     if(nums[0] == -1 && nums[nums.size()-1] == (0-nums.size()))
+        //         ans = 0;
+        //     else if(nums[0] == 1 && nums[nums.size()-1] == (nums.size()))
+        //         ans = 0;
+        // }
+        return ans;
+    }
+};
